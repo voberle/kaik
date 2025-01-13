1c7e1e4e8b01c58e4744fa61e664517e5729e941
Vincent Oberle <vincent@oberle.org>
2025-01-14 00:34:06 +0200

    Fix colors module name

--- src/common.rs
+++ src/common.rs
@@ -2,11 +2,11 @@
 //! Should be mainly enums and such things, with some utils. No actual logic.
 //! No dependencies on other parts of the project.
 
-mod color;
+mod colors;
 mod pieces;
 mod squares;
 
-pub use color::Color;
+pub use colors::Color;
 pub use pieces::Piece;
 pub use pieces::PieceListBoard;
 pub use squares::Square;
