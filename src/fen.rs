539c74651b50341d7015a2a4c38795665f910798
Vincent Oberle <vincent@oberle.org>
2025-01-22 21:35:33 +0200

    Add more interesting FENs; Cleanup main

--- src/fen.rs
+++ src/fen.rs
@@ -8,11 +8,22 @@ use crate::common::Color;
 use crate::common::Square;
 use crate::common::{Piece, PieceListBoard};
 
-// More on <https://www.chessprogramming.org/Perft_Results>
 pub const EMPTY_BOARD: &str = "8/8/8/8/8/8/8/8 w - - 0 1";
+
+// Interesting positions. <https://www.chessprogramming.org/Perft_Results>
+// Depth 7 - 3195901860 nodes.
 pub const START_POSITION: &str = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
-pub const TRICKY_POSITION: &str =
-    "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1";
+// Position 2. Depth 6 - 8031647685 nodes.
+pub const KIWIPETE: &str = "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1";
+// Depth 7 - 178633661 nodes.
+pub const POSITION_3: &str = "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1";
+// Depth 6 - 706045033 nodes.
+pub const POSITION_4: &str = "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1";
+// Depth 5 - 89941194 nodes.
+pub const POSITION_5: &str = "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8";
+// Depth 6 - 6923051137 nodes.
+pub const POSITION_6: &str =
+    "r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10";
 
 fn create_rank(rank: &[Option<Piece>]) -> String {
     assert_eq!(rank.len(), 8);
--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,7 @@ fn main() {
     use common::Square::*;
 
     let mut b = Board::initial_board();
+    // let mut b: Board = "".into();
 
     let args: Vec<String> = env::args().collect();
     if args.len() >= 2 {
@@ -25,7 +26,6 @@ fn main() {
         let moves = args.get(2).map_or("", |v| v.as_str());
 
         apply_moves(&mut b, moves);
-        // b.print();
         divide(&b, depth);
 
         return;
@@ -37,35 +37,6 @@ fn main() {
     b.print();
     println!();
 
-    // let b: Board = "rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 1 2".into();
-    // b.print();
-    // println!("FEN: {b}");
-
-    // let b: Board = "2k5/8/8/8/8/8/2Pp4/2K5 w - - 0 1".into();
-    // let b: Board = "2k5/2Pp4/8/8/8/8/8/2K5 b - - 0 1".into();
-    // let b: Board = "4k3/8/6p1/3P4/5N2/1N6/8/R3K3 w - - 0 1".into();
-    // let moves = b.generate_moves();
-
-    // let b: Board = "8/8/8/8/4N3/n1pB2P1/PPPPPPPP/8 w - - 0 1".into();
-    // let moves = b.generate_moves_for(&[Piece::WhitePawn]);
-    // let b: Board = "8/pppppppp/n1pB2P1/4N3/8/8/8/8 b - - 0 1".into();
-    // let moves = b.generate_moves_for(&[Piece::BlackPawn]);
-
-    // let b: Board = fen::TRICKY_POSITION.into();
-    // let moves = b.generate_moves();
-
-    // let b: Board = "2r3k1/1q1nbppp/r3p3/3pP3/pPpP4/P1Q2N2/2RN1PPP/2R4K b - b3 0 23".into();
-    // let moves = b.generate_moves_for(&[Piece::BlackPawn]);
-
-    // let b: Board = "8/8/8/8/8/8/8/R3K2R w KQkq - 0 1".into(); // Castling
-    // let moves = b.generate_moves_for(&[]);
-
-    // let b: Board = "4k3/1P6/8/8/8/8/8/4K3 w - - 0 1".into(); // Promotion
-    // let moves = b.generate_moves_for(&[Piece::WhitePawn]);
-
-    // apply_moves(&mut b, "a2a4 a7a6 a4a5 b7b5 a5b6");
-    // b.print();
-
     let moves = b.generate_moves();
     print_moves_with_board(&b, &moves);
     print_moves_statistics(&moves);
@@ -73,27 +44,6 @@ fn main() {
     for mv in &moves {
         println!("{}", mv.pure());
     }
-
-    // King attacks
-    // let b: Board = "rnbqk2r/pppp1ppp/8/8/1b2PP1P/1PP5/P2p1P2/RNBQKBNR w KQkq - 0 10".into();
-    // let b: Board = "rnbqk2r/pppp1ppp/8/3P4/1b2PP1P/1P6/P4P2/RNBQKBNR w KQkq - 0 10".into();
-    // b.print();
-    // let bb = b.attacks_king(common::Color::White);
-    // bb.print();
-
-    // let mut b = Board::initial_board();
-    // b.update_by_move(b.new_move(B1, C3));
-    // b.update_by_move(b.new_move(D7, D5));
-    // b.update_by_move(b.new_move(C3, D5));
-
-    // b.print();
-    // divide(&b, 1);
-
-    // b.print_bitboards();
-
-    // let moves = b.generate_moves();
-    // print_moves_with_board(&b, &moves);
-    // print_moves_statistics(&moves);
 }
 
 fn perft(board: &Board, depth: usize) {
@@ -103,8 +53,6 @@ fn perft(board: &Board, depth: usize) {
 
 fn divide(board: &Board, depth: usize) {
     // Output format is the same as Stockfish "go perft <depth>" command.
-    // Save both outputs to p_stockfish and p_kaik, and compare with:
-    //   diff <(sort p_stockfish) <(sort p_kaik)
     let nodes = board.divide(depth);
     for (mv, count) in &nodes {
         println!("{}: {count}", mv.pure());
